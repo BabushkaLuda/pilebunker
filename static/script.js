a210e7f11680c73ee6e3169671cc46dabb338a46
BabushkaLuda <drachev_yv_20@mf.grsu.by>
2025-02-18 09:05:39 +0300

    some tweaks
    - added script.js with scripts
    - implemented accordion

---
+++ static/script.js
@@ -0,0 +1,23 @@
+function play() {
+	var num = Math.floor(Math.random() * 4) + 1;
+	var audio = new Audio('media/' + num.toString() + '.ogg');
+	audio.volume = 0.1;
+	audio.play();
+}
+
+window.onload = function() {
+	var acc = document.getElementsByClassName("accordion");
+	var i;
+	for (i = 0; i < acc.length; i++) {
+		acc[i].addEventListener("click", function() {
+			this.classList.toggle("active");
+			var panel = this.nextElementSibling;
+			if (panel.style.display === "block") {
+				panel.style.display = "none";
+			}
+			else {
+				panel.style.display = "block";
+			}
+		});
+	}
+}
